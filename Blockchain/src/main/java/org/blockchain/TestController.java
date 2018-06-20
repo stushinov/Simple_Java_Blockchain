824581bbc98c1bf055ac6b92dfcf8407f996a3fe
Stoyan Tushinov <tanuka@mail.bg>
2018-06-20 13:17:01 +0300

    Added test controller to see if the files to be ignore are actually ignored(.idea)

---
+++ Blockchain/src/main/java/org/blockchain/TestController.java
@@ -0,0 +1,16 @@
+package org.blockchain;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String index(){
+
+        return"<h1>Hello world</h1>";
+    }
+}
