48c0bf591f8aa7f5fc5be9205e2531ff8cbc42cd
Stoyan Tushinov <tanuka@mail.bg>
2018-06-21 16:05:49 +0300

    Added more tests to the blockchain object

--- Blockchain/src/test/java/org/blockchain/BlockchainTests.java
+++ Blockchain/src/test/java/org/blockchain/BlockchainTests.java
@@ -5,9 +5,9 @@ import org.blockchain.core.Blockchain;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.math.BigInteger;
 
 public class BlockchainTests {
 
@@ -16,6 +16,49 @@ public class BlockchainTests {
     @Before
     public void initBlockchain(){
         this.blockchain = new Blockchain();
+    }
+
+    @Test
+    public void creationOfANewBlockShouldExtractThePendingTransactionsInsideIt() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+
+        this.assertPendingTransactionsAreEmpty(this.blockchain);
+
+        this.seedTransactions(this.blockchain);
+        final long PROOF_MOCK = 4;
+        this.blockchain.newBlock(PROOF_MOCK);
+        Block block = getLastBlock(this.blockchain);
+
+        this.assertPendingTransactionsAreEmpty(this.blockchain);
+
+        final long TRANSACTIONS_EXPECTED = 4;
+        final long TRANSACTIONS_FOUND = block.getTransactions().size();
+
+        Assert.assertEquals("Transaction count mismatch!", TRANSACTIONS_EXPECTED, TRANSACTIONS_FOUND);
+    }
+
+    @Test
+    public void callingNewTransactionMethodShouldAddTransactionToPendingTransactions(){
+
+        this.assertPendingTransactionsAreEmpty(this.blockchain);
+
+        this.seedTransactions(this.blockchain);
+
+        final long EXPECTED_TRANSACTIONS_COUNT_AFTER_SEEDING = 4;
+        final long ACTUAL_TRANSACTION_COUNT = this.blockchain.getPendingTransactions().size();
+
+        Assert.assertEquals("Transactions count does not match!",
+                EXPECTED_TRANSACTIONS_COUNT_AFTER_SEEDING,
+                ACTUAL_TRANSACTION_COUNT);
+    }
+
+    private void assertPendingTransactionsAreEmpty(Blockchain blockchain) {
+        //Check if pending transactions are empty.
+        final long EXPECTED_BEGINNING_TRANSACTIONS_COUNT = 0;
+        final long BEGINNING_TRANSACTIONS_COUNT = blockchain.getPendingTransactions().size();
+
+        Assert.assertEquals("Transactions are not empty!",
+                EXPECTED_BEGINNING_TRANSACTIONS_COUNT,
+                BEGINNING_TRANSACTIONS_COUNT);
     }
 
     @Test
@@ -47,5 +90,12 @@ public class BlockchainTests {
         Method method = blockchain.getClass().getDeclaredMethod("getLastBlock");
         method.setAccessible(true);
         return (Block) method.invoke(blockchain, new Class[]{});
+    }
+
+    private void seedTransactions(Blockchain blockchain) {
+        blockchain.newTransaction("1", "4", BigInteger.valueOf(20));
+        blockchain.newTransaction("2", "3", BigInteger.valueOf(20));
+        blockchain.newTransaction("3", "2", BigInteger.valueOf(20));
+        blockchain.newTransaction("4", "1", BigInteger.valueOf(20));
     }
 }
